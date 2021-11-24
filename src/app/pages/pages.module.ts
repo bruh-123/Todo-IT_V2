c477aa72c4dd72838a609ec8906e8d0e907c87a9
BRUNO RIQUELME <brunomriquelme@gmail.com>
2021-11-24 20:01:12 -0300

    paginas creadas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,9 +5,12 @@ import { ComponentsModule } from '../components/components.module';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from '../material.module';
+import { TripComponent } from './trip/trip.component';
+import { StateComponent } from './state/state.component';
+import { RecordComponent } from './record/record.component';
 
 @NgModule({
-  declarations: [HomeComponent, SignupComponent, LoginComponent],
-  imports: [CommonModule, ComponentsModule, MaterialModule],
+  declarations: [HomeComponent, SignupComponent, LoginComponent, TripComponent, StateComponent, RecordComponent],
+  imports: [CommonModule, ComponentsModule, MaterialModule,],
 })
 export class PagesModule {}
