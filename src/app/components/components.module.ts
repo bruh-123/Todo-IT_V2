aa96572672c29e0ff36b5a56423893cc20b730f9
BRUNO RIQUELME <brunomriquelme@gmail.com>
2021-11-26 17:35:25 -0300

    mergin footer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,11 +4,10 @@ import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 import { MaterialModule } from '../material.module';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [InicioSesionComponent],
+  declarations: [InicioSesionComponent, FooterComponent],
   imports: [CommonModule, MaterialModule, RouterModule, ReactiveFormsModule],
   exports: [InicioSesionComponent],
 })
