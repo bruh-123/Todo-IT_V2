a3091c40bb322c65239e3e766bb36c5be8dac78e
BRUNO RIQUELME <brunomriquelme@gmail.com>
2021-12-05 19:44:04 -0300

    removing testing files .spec

--- src/app/Pipes/fecha-entrega.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FechaEntregaPipe } from './fecha-entrega.pipe';
-
-describe('FechaEntregaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FechaEntregaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
